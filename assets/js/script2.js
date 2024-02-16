6ce35488fef80f679353030eda9dedd201dbd2d6
Alejandro Silva Quiñones <janosilva@gmail.com>
2024-02-15 22:01:29 -0300

    avances ejercicio 2, termino dia 1

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -9,8 +9,18 @@
 //         caja.computedStyleMap.color = color;
 //     }
 // */
+/*Para cambiar el color de fondo al hacer clic con 
+JavaScript, se puede conservar el color de la variable 
+bg.color en el container*/
 
-const boton1 = document.getElementById("btn-1")
-boton1.addEventListener("click",()=>{
-    
-})
+const caja = document.getElementById("caja");
+document.addEventListener("click", pintar());
+
+function pintar() {
+    document.getElementById("caja").style.backgroundColor = "#e53e3e";
+    document.getElementById("caja").style.backgroundColor = "#dd6b20";
+    document.getElementById("caja").style.backgroundColor = "#faf089";
+    document.getElementById("caja").style.backgroundColor = "#48bb78";
+    document.getElementById("caja").style.backgroundColor = "#81e6d9";
+    document.getElementById("caja").style.backgroundColor = "#d53f8c";
+}
